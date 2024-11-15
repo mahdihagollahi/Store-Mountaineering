3c5707b01330e1550f3608361ef30ab9feb383ad
mahdihaghollahi <mahdiaghollahi@gmail.com>
2024-11-15 18:37:26 +0330

    fix product detail

---
+++ src/component/Products/ProductDetail/ImageProduct.tsx
@@ -0,0 +1,95 @@
+import Image from "next/image";
+import React, { useState } from "react";
+import chevronRightImage from "@/assent/Imge/Product/chevronRightImage.svg";
+import chevronLeftImage from "@/assent/Imge/Product/chevronLeftImage.svg";
+
+const ImageGallery = ({ Images }: { Images: string[] }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleThumbnailClick = (index: number) => {
+    setCurrentIndex(index);
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % Images.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + Images.length) % Images.length
+    );
+  };
+
+  console.log("Images:", Images);
+
+  return (
+    <div className="image-gallery">
+
+      <div className="main-image-container ">
+        <Image
+          src={Images[currentIndex]}
+          alt="Main image"
+          className=""
+          style={{ width: "615px", height: "496px" }}
+        />
+      </div>
+
+ 
+      <div className="navigation">
+       
+
+      
+      </div>
+
+      <div className="flex thumbnails items-center">
+      <div onClick={handleNext}>
+          <Image src={chevronRightImage} width={24} height={24} alt="" />
+        </div>
+      
+        <div className="thumbnails">
+          {Images.map((image, index) => (
+            <Image
+              key={index}
+              src={image}
+              alt={`Thumbnail ${index + 1}`}
+              className={`thumbnail ${index === currentIndex ? "active" : ""}`}
+              onClick={() => handleThumbnailClick(index)}
+              width={100}
+              height={80}
+            />
+          ))}
+        </div>
+        <div onClick={handlePrev}>
+          <Image src={chevronLeftImage} width={24} height={24} alt="" />
+        </div>
+      </div>
+
+      <style jsx>{`
+        .image-gallery {
+          text-align: center;
+        }
+        .main-image-container {
+          width: 600px;
+          margin: auto;
+        }
+        .navigation {
+          margin: 10px 0;
+        }
+        .thumbnails {
+          display: flex;
+          justify-content: center;
+          gap: 10px;
+        }
+        .thumbnail {
+          cursor: pointer;
+          border: 2px solid transparent;
+        }
+        .thumbnail.active {
+          border-color: blue;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default ImageGallery;
---
+++ src/component/Products/ProductDetail/ProductDetail.tsx
@@ -0,0 +1,78 @@
+"use client";
+import React, { Fragment, useState } from "react";
+import ProductDetailData from "./ProductDetailData";
+import Image from "next/image";
+import Color from "@/assent/Imge/Product/ProductDetail/color.svg";
+import Plus from "@/assent/Imge/Product/ProductDetail/Plus.svg";
+import Link from "next/link";
+import ImageGallery from "./ImageProduct";
+
+
+
+const ProductDetail = () => {
+  const [counter, setCounter] = useState(0);
+  const { ProductDetail } = ProductDetailData();
+  const product = ProductDetail[0];
+
+  function posetive() {
+    setCounter(counter + 1);
+  }
+  function negtiv() {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  }
+ 
+  return (
+    <Fragment>
+      <div className="px-20">
+        <div className="flex mt-32 justify-between ">
+          <div>
+            <p className="font-demiBold text-xl text-[#3A3A3A] leading-8">
+              {product.productTitle}
+            </p>
+
+            <p className="font-medium mt-9 text-base text-[#727272] leading-7">
+              {product.productDetail}
+            </p>
+
+            <p className="font-medium mt-12 text-lg text-[#3A3A3A] leading-7">
+              <span className="text-[#727272]">قیمت :</span> {product.price}{" "}
+              تومان
+            </p>
+            <p className="font-medium mt-12 text-base text-[#727272] leading-7">
+              انتخاب رنگ :
+            </p>
+            <Image
+              src={Color}
+              width={191}
+              height={27}
+              alt=""
+              className="mt-3"
+            />
+            <div className="flex gap-6 items-center mt-12 ">
+              <p className="text-[#727272] text-base font-medium mt-9">
+                تعداد:
+              </p>
+              <div className="flex flex-row mt-9 gap-4">
+                <Image onClick={() => posetive()} src={Plus} alt="" />
+                <span>{counter}</span>
+                <Image onClick={() => negtiv()} src={Plus} alt="" />
+              </div>
+            </div>
+            <Link href="/Resevaion/SetTime">
+              <button className="mt-10 duration-300 bg-[#54896b] hover:bg-[#2E8B57] text-white w-[98%] h-12 rounded-xl">
+              افزودن به سبد خرید
+              </button>
+            </Link>
+          </div>
+          <div className="xl:-mt-10">
+            <ImageGallery Images={product.images}/>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default ProductDetail;
