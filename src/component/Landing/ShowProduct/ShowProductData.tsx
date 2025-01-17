caef63fbab59a04c9b091bb15c058edaa8f8fdc2
mahdihaghollahi <mahdiaghollahi@gmail.com>
2025-01-17 16:28:02 +0330

    add show product in landing

---
+++ src/component/Landing/ShowProduct/ShowProductData.tsx
@@ -0,0 +1,40 @@
+import Chador from "@/assent/Imge/Landing/ShowProduct/Chador.svg";
+const ShowProductData = () => {
+  return {
+    Product: [
+      {
+        id: 1,
+        name: "ابزار فنی کوه نوردی ",
+        src: Chador,
+      },
+
+      {
+        id: 2,
+        name: "ابزار فنی کوه نوردی ",
+        src: Chador,
+      },
+
+
+      {
+        id: 3,
+        name: "ابزار فنی کوه نوردی ",
+        src: Chador,
+      },
+
+
+      {
+        id: 4,
+        name: "ابزار فنی کوه نوردی ",
+        src: Chador,
+      },
+
+
+    
+
+    
+
+    ],
+  };
+};
+
+export default ShowProductData;
